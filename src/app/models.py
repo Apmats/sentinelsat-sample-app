8cbf16e30a87cd01902a705620d54668b5d8fdb6
Apostolos <amatsagkas@gmail.com>
2023-08-28 14:42:55 +0300

    Removing mistakenly added default

--- src/app/models.py
+++ src/app/models.py
@@ -3,8 +3,8 @@ from sqlmodel import SQLModel, Field
 
 class BoundingBox(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    min_x: float = Field(default=None, index=True)
-    min_y: float = Field(default=None, index=True)
-    max_x: float = Field(default=None, index=True)
-    max_y: float = Field(default=None, index=True)
+    min_x: float = Field(index=True)
+    min_y: float = Field(index=True)
+    max_x: float = Field(index=True)
+    max_y: float = Field(index=True)
     image_path: str
